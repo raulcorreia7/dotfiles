66580ca8b0305a73d201ddfee5b582bd577c29cd
Raúl Correia <raulcorreia7@users.noreply.github.com>
2026-03-14 03:43:35 +0100

    fix(zimfw): use user zimfw path and add gitignore for generated files

--- config/zimfw/init.zsh
+++ config/zimfw/init.zsh
@@ -1,7 +1,7 @@
 # FILE AUTOMATICALLY GENERATED FROM /home/rcorreia/.dotfiles/config/.zimrc
 # EDIT THE SOURCE FILE AND THEN RUN zimfw build. DO NOT DIRECTLY EDIT THIS FILE!
 
-if [[ -e ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]] zimfw() { source "/usr/share/zimfw/zimfw.zsh" "${@}" }
+if [[ -e ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]] zimfw() { source "${HOME}/.zim/zimfw.zsh" "${@}" }
 fpath=("${HOME}/.dotfiles/config/zimfw/modules/git/functions" "${HOME}/.dotfiles/config/zimfw/modules/utility/functions" "${HOME}/.dotfiles/config/zimfw/modules/duration-info/functions" "${HOME}/.dotfiles/config/zimfw/modules/git-info/functions" "${HOME}/.dotfiles/config/zimfw/modules/zsh-completions/src" "${HOME}/.dotfiles/config/zimfw/modules/completion/functions" ${fpath})
 autoload -Uz -- git-alias-lookup git-branch-current git-branch-delete-interactive git-branch-remote-tracking git-dir git-ignore-add git-root git-stash-clear-interactive git-stash-recover git-submodule-move git-submodule-remove mkcd mkpw duration-info-precmd duration-info-preexec coalesce git-action git-info
 source "${HOME}/.dotfiles/config/zimfw/modules/environment/init.zsh"
